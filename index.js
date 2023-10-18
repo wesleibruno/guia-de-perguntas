e2328b3ebdf8b3862bc3e58be899797a9956a3fc
weslei bruno de santana <weslei945@gmail.com>
2023-10-18 09:48:54 -0300

    connection with database, using sequelize

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+//Database
 
-//Estou dizendo pra o Express utilizar o EJS como engine
-app.set('view engine', 'ejs');
-
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-  res.render('index.ejs', {
-    nome,
-    lang,
-    empresa: "teste"
-  })
+connection
+.authenticate()
+.then(() => {
+  console.log("Banco de dados Autenticado com sucesso")
+})
+.catch((msgErro) => {
+    console.log(msgErro)
 });
 
-app.listen(8080, () => {
-  console.log("APP RODANDO");
+//Estou dizendo pra o Express utilizar o EJS como engine
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Rotas 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar.ejs");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+  res.send(`${titulo} ${descricao}`);
+});
+const port = 8080
+app.listen(port, () => {
+  console.log(`APP RODANDO http://localhost:${port}`);
 });
